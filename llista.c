78edfb319d3ca714f97169de07bf68398726bde4
alvaro.castillejo <alvaro.castillejo@students.salle.url.edu>
2018-10-07 21:36:39 +0200

    doc: Documentation on the "llista.c" file.
    Included all the headlines with the explanation of what each function
    do, which are their parameters and what each function returns.

--- llista.c
+++ llista.c
@@ -1,6 +1,11 @@
 #include "llista.h"
 
-LlistaBi LLISTABI_crea () {
+/****************************************
+*@Finalitat: crea una llista del no res i afegeix un element fantasma
+*@Parametres: -
+*@Retorn: l
+****************************************/
+LlistaBi LLISTABI_crea () {										
 	LlistaBi l; 
 
     l.pri = (NodeBi*) malloc (sizeof(NodeBi));  
@@ -23,6 +28,11 @@ LlistaBi LLISTABI_crea () {
     return l;     
 }
 
+/****************************************
+*@Finalitat: Consulta la casella del PDI.
+*@Parametres: "LlistaBi l" La llista d'on consultar.
+*@Retorn: Element
+****************************************/
 Element LLISTABI_consulta (LlistaBi l) {
     Element e=ELEMENT_indefinit;
     if (l.pri->seg==NULL && l.ult->ant==NULL) {
@@ -33,22 +43,47 @@ Element LLISTABI_consulta (LlistaBi l) {
     return e;
 }
 
+/****************************************
+*@Finalitat: Porta el PDI al inici de la llista, es a dir, a l'element d'acontinuacio del fantasma
+*@Parametres: "LlistaBi *l" llista sobre la que operar.
+*@Retorn: void
+****************************************/
 void LLISTABI_VesInici (LlistaBi *l) {
     l->pdi=l->pri->seg;
 }
 
+/****************************************
+*@Finalitat: Porta el PDI al final de la llista
+*@Parametres: "LlistaBi *l" llista sobre la que operar.
+*@Retorn: void
+****************************************/
 void LLISTABI_VesFinal (LlistaBi *l) {
     l->pdi=l->ult->ant;
 }
 
+/****************************************
+*@Finalitat: Comprova si el PDI es al principi.
+*@Parametres: "LlistaBi l" la llista on comprovar.
+*@Retorn: int
+****************************************/
 int LLISTABI_inici(LlistaBi l) {
     return l.pdi==l.pri;
 }
 
+/****************************************
+*@Finalitat: Comprova si el PDI es al final.
+*@Parametres: "LlistaBi l" la llista on comprovar.
+*@Retorn: int
+****************************************/
 int LLISTABI_final(LlistaBi l) {
     return l.pdi==l.ult;
 }
 
+/****************************************
+*@Finalitat: elimina tots els elements de la llista alliberant la memoria dinamica que ocupen.
+*@Parametres: "LlistaBi *l" llista a eliminar 
+*@Retorn: void 
+****************************************/
 void LLISTABI_destrueixl(LlistaBi *l) {
     NodeBi *aux;
     aux=(NodeBi*)malloc(sizeof(NodeBi));
@@ -65,6 +100,11 @@ void LLISTABI_destrueixl(LlistaBi *l) {
     }
 }
 
+/****************************************
+*@Finalitat: Esborra un element de la llista. Aquest element sera el PDI.
+*@Parametres: "LlistaBi *l" llista d'on borrar. 	
+*@Retorn: void
+****************************************/
 void LLISTABI_esborra(LlistaBi *l) {
     NodeBi *aux;
     if (l->pri->seg==NULL || l->ult->ant==NULL) {
@@ -78,6 +118,11 @@ void LLISTABI_esborra(LlistaBi *l) {
     }
 }
 
+/****************************************
+*@Finalitat: avanca una posicio el PDI de la llista. 
+*@Parametres: "LlistaBi *l" llista on avancar
+*@Retorn: void 
+****************************************/
 void LLISTABI_avanca (LlistaBi *l) {
     if (l->pdi->seg==NULL) {
         //Error
@@ -86,6 +131,11 @@ void LLISTABI_avanca (LlistaBi *l) {
     }
 }
 
+/****************************************
+*@Finalitat: retrocedeix una posicio el PDI de la llista. 
+*@Parametres: "LlistaBi *l" llista on avancar
+*@Retorn: void 
+****************************************/
 void LLISTABI_retocedeix(LlistaBi*l) {
     if (l->pdi->ant==NULL) {
         //Error
@@ -94,10 +144,21 @@ void LLISTABI_retocedeix(LlistaBi*l) {
     }
 }
 
+/****************************************
+*@Finalitat: Comprova si la llista es buida, es a dir, que nomes conte l'element fantasma.
+*@Parametres: "LlistaBi l" La llista a comprovar. 
+*@Retorn: int que indica amb un 1 si esta buida
+****************************************/
 int LLISTABI_buida (LlistaBi l) {
     return l.pri->seg==l.ult;
 }
 
+/****************************************
+*@Finalitat: insereix un nou element a la llista a l'esquerra d'el PDI.
+*@Parametres: 	"LlistaBi *l" Llista on inserir. 
+*				"Element e" Element a inserir a la llista. 
+*@Retorn: void
+****************************************/
 void LLISTABI_insereixDavant (LlistaBi *l, Element e) {
 	NodeBi *aux; 
 	aux=(NodeBi*)malloc(sizeof(NodeBi)); 
@@ -116,6 +177,12 @@ void LLISTABI_insereixDavant (LlistaBi *l, Element e) {
 	}
 }
 
+/****************************************
+*@Finalitat: insereix un nou element a la llista tot habent colocat el PDI on pertoca.
+*@Parametres: 	"Element e" Element a inserir a la llista. 
+*				"LlistaBi *l" Llista on inserir. 
+*@Retorn: void
+****************************************/
 void LLISTABI_insereixOrdenat (Element e, LlistaBi *l) {
 	NodeBi *aux; 
 	aux=(NodeBi*)malloc(sizeof(NodeBi)); 
