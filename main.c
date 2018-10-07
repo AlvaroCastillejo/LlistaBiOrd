1dcf2757386bb87ea71a3882a2743cf5b49589de
alvaro.castillejo <alvaro.castillejo@students.salle.url.edu>
2018-10-07 19:55:10 +0200

    feat: Created the mechanism that shows the sorted list.
    Included two for-loops. The first one shows the elements of each box
    starting from the beggining to the end of the list. The second for-loop
    shows all the elements of the list but starting from the end to the
    beggining of it. Thats possible because it is a bidirectional linked
    list.

--- main.c
+++ main.c
@@ -18,4 +18,21 @@ void main() {
 		fflushnou();
 	    LLISTABI_insereixOrdenat(element, &l);  
 	}
+
+	printf("Aquesta es la llista ordenada: \n");
+
+	LLISTABI_VesInici (&l); 
+	for(i=0;i<=n;i++){
+		element = LLISTABI_consulta(l);
+		printf("%d\n", element);
+		LLISTABI_avanca(&l);
+	}
+
+	printf("Aquesta es la llista ordenada a la inversa: \n");
+
+	LLISTABI_VesFinal (&l); 
+	for(i=0;i<=n;i++){
+		element = LLISTABI_consulta(l);
+		printf("%d\n", element);
+		LLISTABI_retocedeix(&l); 
 }
