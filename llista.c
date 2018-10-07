319788bdca5a05bb320567767f576b6cd2464eaa
alvaro.castillejo <alvaro.castillejo@students.salle.url.edu>
2018-10-07 19:10:47 +0200

    feat: Created empty patterns for all the functions needed.
    Created empty patterns for the functions. All the functions have been
    created due the llista.h file.

--- llista.c
+++ llista.c
@@ -1,1 +1,53 @@
 #include "llista.h"
+
+LlistaBi LLISTABI_crea () {
+
+}
+
+Element LLISTABI_consulta (LlistaBi l) {
+
+}
+
+void LLISTABI_VesInici (LlistaBi *l) {
+
+}
+
+void LLISTABI_VesFinal (LlistaBi *l) {
+
+}
+
+int LLISTABI_inici(LlistaBi l) {
+
+}
+
+int LLISTABI_final(LlistaBi l) {
+
+}
+
+void LLISTABI_destrueixl(LlistaBi *l) {
+
+}
+
+void LLISTABI_esborra(LlistaBi *l) {
+
+}
+
+void LLISTABI_avanca (LlistaBi *l) {
+
+}
+
+void LLISTABI_retocedeix(LlistaBi*l) {
+
+}
+
+int LLISTABI_buida (LlistaBi l) {
+
+}
+
+void LLISTABI_insereixDavant (LlistaBi *l, Element e) {
+
+}
+
+void LLISTABI_insereixOrdenat (Element e, LlistaBi *l) {
+
+}
