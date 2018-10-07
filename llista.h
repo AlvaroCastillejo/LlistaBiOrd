596fab9a8f730bd35198423ad31ad5b794a5ab6d
alvaro.castillejo <alvaro.castillejo@students.salle.url.edu>
2018-10-07 18:53:16 +0200

    feat: Included (in llista.h) all the functions the will be used.
    In the "llista.h" file, all the functions of the library had been
    included.

--- llista.h
+++ llista.h
@@ -20,4 +20,19 @@ typedef struct {
 	NodeBi *pdi; 
 } LlistaBi; 
 
+LlistaBi LLISTABI_crea(); 
+Element LLISTABI_consulta (LlistaBi l); 
+void LLISTABI_esborra (LlistaBi *l); 
+void LLISTABI_avanca (LlistaBi *l); 
+void LLISTABI_retocedeix (LlistaBi *l); 
+int LLISTABI_buida (LlistaBi l); 
+void LLISTABI_VesInici (LlistaBi *l); 
+void LLISTABI_VesFinal (LlistaBi *l); 
+int LLISTABI_inici (LlistaBi l); 
+int LLISTABI_final (LlistaBi l); 
+void LLISTABI_destrueix (LlistaBi *l); 
+void LLISTABI_insereixDavant (LlistaBi *l, Element e); 
+void LLISTABI_insereixOrdenat(Element e, LlistaBi *l);
+
+
 #endif
