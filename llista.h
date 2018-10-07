3a363d81219038d2678b0338379eee86722a4c5d
alvaro.castillejo <alvaro.castillejo@students.salle.url.edu>
2018-10-07 18:50:45 +0200

    feat: Included the structures needed to hold the program.
    Created the different structures in which our code will be based.

--- llista.h
+++ llista.h
@@ -4,4 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ELEMENT_indefinit -1
+
+typedef int Element;
+
+typedef struct N { 
+	Element e; 
+	struct N *seg; 
+	struct N *ant; 
+} NodeBi; 
+
+typedef struct { 
+	NodeBi *pri; 
+	NodeBi *ult; 
+	NodeBi *pdi; 
+} LlistaBi; 
+
 #endif
